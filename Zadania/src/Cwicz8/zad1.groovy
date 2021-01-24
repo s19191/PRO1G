740eb93c256f1f44432c22530ac1c5396b122f90
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-01-25 00:09:33 +0100

    zrobione do Cwicz9 bez Cwicz9 i bez Cwicz2 zad6

--- Zadania/src/Cwicz8/zad1.groovy
+++ Zadania/src/Cwicz8/zad1.groovy
@@ -1,2 +1,7 @@
 package Cwicz8
 
+import static javax.swing.JOptionPane.showInputDialog
+
+def array = (showInputDialog("Podaj ciąg liczb") as List).collect({it as Integer})*.multiply(2)
+
+println array
