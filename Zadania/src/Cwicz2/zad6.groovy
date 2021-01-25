aee95533341c3a8f9af95995445ec09e7d8bf0f8
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-01-25 04:25:07 +0100

    nie jakoś zabójczo mądre, ale coś robi

--- Zadania/src/Cwicz2/zad6.groovy
+++ Zadania/src/Cwicz2/zad6.groovy
@@ -1,1 +1,28 @@
 package Cwicz2
+
+def counter = 1
+def fib
+fib = { n ->
+    counter++
+    if (n < 2) {
+        n
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}
+assert fib(15)
+println counter
+
+counter = 1
+fib = { n ->
+    counter++
+    if (n < 2) {
+        n
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}.memoize()
+
+assert fib(25)
+
+println counter
